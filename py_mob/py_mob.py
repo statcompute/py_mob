cedba6988285b3e4dda1d97f20c8d8ecbfaf3e6c
WenSui Liu <liuwensui@gmail.com>
2020-05-16 09:29:17 -0500

    Add files via upload

---
+++ py_mob/py_mob.py
@@ -0,0 +1,435 @@
+import numpy, scipy.stats, sklearn.isotonic, tabulate
+
+###################################################################################################
+
+def summ_bin(x):
+  """
+  The function summarizes the binning outcome generated from a binning function, e.g. qtl_bin() or
+  iso_bin().
+
+  Parameters:
+    x: An object containing the binning outcome.
+
+  Returns:
+    A dictionary with statistics derived from the binning outcome
+
+  Example:
+    summ_bin(iso_bin(ltv, bad))
+    # {'bad rate': 0.2049, 'iv': 0.18, 'ks': 16.88}
+  """
+ 
+  _r1 = round(sum([_['bads'] for _ in x['tbl']]) / sum([_['freq'] for _ in x['tbl']]), 4)
+  _r2 = sum([_['iv'] for _ in x['tbl']])
+
+  _freq = sum([_['freq'] for _ in x['tbl']])
+  _bads = sum([_['bads'] for _ in x['tbl']])
+
+  cumsum = lambda x: [sum([_ for _ in x][0:(i+1)]) for i in range(len(x))]
+  _cumb = cumsum([_['bads'] / _bads for _ in x['tbl']])
+  _cumg = cumsum([(_['freq'] - _['bads']) / (_freq - _bads) for _ in x['tbl']])
+  _r3 = round(max([numpy.abs(_[0] - _[1]) for _ in zip(_cumb, _cumg)]) * 100, 2)
+
+  return({"bad rate": _r1, "iv": _r2, "ks": _r3})
+
+###################################################################################################
+
+def view_bin(x):
+  """
+  The function shows the binning outcome generated from a binning function, e.g. qtl_bin() or iso_bin().
+
+  Parameters:
+    x: An object containing the binning outcome.
+
+  Returns:
+    None
+
+  Example:
+    view_bin(iso_bin(ltv, bad))
+
+    |   bin |   freq |   miss |   bads |   rate |     woe |     iv | rule                         |
+    |-------+--------+--------+--------+--------+---------+--------+------------------------------|
+    |     0 |    213 |    213 |     70 | 0.3286 |  0.6416 | 0.0178 | numpy.isnan($X$)             |
+    |     1 |   2850 |      0 |    367 | 0.1288 | -0.5559 | 0.1268 | $X$ <= 0.0                   |
+    |     2 |    891 |      0 |    193 | 0.2166 |  0.0704 | 0.0008 | ($X$ > 0.0) and ($X$ <= 1.0) |
+    |     3 |    810 |      0 |    207 | 0.2556 |  0.2867 | 0.0124 | ($X$ > 1.0) and ($X$ <= 3.0) |
+    |     4 |   1073 |      0 |    359 | 0.3346 |  0.6684 | 0.0978 | $X$ > 3.0                    |
+
+  """
+
+  print(tabulate.tabulate(x['tbl'], headers = "keys", tablefmt = "orgtbl"))
+
+###################################################################################################
+
+def qcut(x, n):
+  """
+  The function discretizes a numeric vector into n pieces based on quantiles.
+
+  Parameters:
+    x: A numeric vector.
+    n: An integer indicating the number of categories to discretize.
+
+  Returns:
+    A list of numeric values to divide the vector x into n categories.
+
+  Example:
+    qcut(range(10), 3)
+    # [3, 6]
+  """
+
+  _q = numpy.linspace(0, 100, n, endpoint = False)[1:]
+  _x = [_ for _ in x if not numpy.isnan(_)]
+  _c = numpy.unique(numpy.percentile(_x, _q, interpolation = "lower"))
+  return([_ for _ in _c])
+
+###################################################################################################
+
+def manual_bin(x, y, cuts):
+  """
+  The function discretizes the x vector and then summarizes over the y vector 
+  based on the discretization result.
+
+  Parameters:
+    x    : A numeric vector to discretize without missing values, e.g. numpy.nan or math.nan
+    y    : A numeric vector with binary values of 0/1 and with the same length of x 
+    cuts : A list of numeric values as cut points to discretize x. 
+
+  Returns:
+    A list of dictionaries for the binning outcome with following keys:
+      "bin"  : the binning group
+      "freq" : the number of records in each binning group
+      "miss" : the placeholder for records with missing values of x
+      "bads" : the number of records for y = 1 in each binning group
+      "minx" : the minimum value of x for each binning group
+      "maxx" : the maximum value of x for each binning group
+
+  Example:
+    for x in manual_bin(scr, bad, [650, 700, 750]):
+      print(x)
+
+  # {'bin': 1, 'freq': 1311, 'miss': 0, 'bads': 520.0, 'minx': 443.0, 'maxx': 650.0}
+  # {'bin': 2, 'freq': 1688, 'miss': 0, 'bads': 372.0, 'minx': 651.0, 'maxx': 700.0}
+  # {'bin': 3, 'freq': 1507, 'miss': 0, 'bads': 157.0, 'minx': 701.0, 'maxx': 750.0}
+  # {'bin': 4, 'freq': 1016, 'miss': 0, 'bads':  42.0, 'minx': 751.0, 'maxx': 848.0}
+  """
+
+  _x = [_ for _ in x]
+  _y = [_ for _ in y]
+  _c = sorted([_ for _ in set(cuts)] + [numpy.NINF, numpy.PINF])
+  _g = numpy.searchsorted(_c, _x).tolist()
+
+  _l1 = list(dict(zip(['g', 'x', 'y'], _)) for _ in zip(_g, _x, _y))
+  _l2 = zip(set(_g), [[l for l in _l1 if l["g"] == g] for g in set(_g)])
+
+  return(sorted([dict(zip(["bin", "freq", "miss", "bads", "minx", "maxx"],
+                          [_1, 
+                           len(_2), 
+                           0,
+                           sum([_["y"] for _ in _2]),
+                           min([_["x"] for _ in _2]),
+                           max([_["x"] for _ in _2])])) for _1, _2 in _l2], 
+                key = lambda x: x["bin"]))
+
+###################################################################################################
+
+def miss_bin(y):
+  """
+  The function summarizes the y vector with binary values of 0/1.
+
+  Parameters:
+    y    : A numeric vector with binary values of 0/1 and with the same length of x
+
+  Returns:
+    A dictionary with following keys:
+      "bin"  : 0 as default 
+      "freq" : the number of records 
+      "miss" : same value as "freq"
+      "bads" : the number of records with y = 1
+      "minx" : nan as default
+      "maxx" : nan as default
+
+  Example:
+    miss_bin(scr):
+  
+  # {'bin': 0, 'freq': 315, 'miss': 315, 'bads': 105.0, 'minx': nan, 'maxx': nan}
+  """
+
+  return({"bin": 0, "freq": len([_ for _ in y]), "miss": len([_ for _ in y]), "bads": sum([_ for _ in y]), 
+          "minx": numpy.nan, "maxx": numpy.nan})
+
+###################################################################################################
+
+def qtl_bin(x, y):
+  """
+  The function discretizes the x vector based on percentiles and then summarizes over the y vector
+  to derive the weight of evidence transformaton (WoE) and information values.
+
+  Parameters:
+    x : A numeric vector to discretize. 
+    y : A numeric vector with binary values of 0/1 and with the same length of x.
+
+  Returns:
+    A dictionary with two keys:
+      "cut" : a numeric vector with cut points applied to the x vector.
+      "tbl" : a list of dictionaries summarizing the binning outcome.
+
+  Example:
+    bin = qtl_bin(derog, bad)
+
+    bin["cut"]
+    #  [0.0, 1.0, 3.0]
+
+    import tabulate
+    print(tabulate.tabulate(bin['tbl'], headers = "keys", tablefmt = "simple")) 
+    #   bin    freq    miss    bads    rate      woe      iv  rule
+    # -----  ------  ------  ------  ------  -------  ------  ---------------------------------
+    #     0     213     213      70  0.3286   0.6416  0.0178  numpy.isnan($X$)
+    #     1    2850       0     367  0.1288  -0.5559  0.1268  $X$ <= 0.0
+    #     2     891       0     193  0.2166   0.0704  0.0008  ($X$ > 0.0) and ($X$ <= 1.0)
+    #     3     810       0     207  0.2556   0.2867  0.0124  ($X$ > 1.0) and ($X$ <= 3.0)
+    #     4    1073       0     359  0.3346   0.6684  0.0978  $X$ > 3.0
+  """
+
+  _data = [_ for _ in zip(x, y, ~numpy.isnan(x))]
+  _freq = len(_data)
+  _bads = sum([_[1] for _ in _data])
+
+  _x = [_[0] for _ in _data if _[2] == 1]
+  _y = [_[1] for _ in _data if _[2] == 1]
+
+  _n = numpy.arange(2, max(3, min(50, len(numpy.unique(_x)) - 1)))
+  _p = numpy.unique([qcut(_x, _) for _ in _n])
+
+  _l1 = [[_, manual_bin(_x, _y, _)] for _ in _p]
+
+  _l2 = [[l[0], min([_["bads"] / _["freq"] for _ in l[1]]), max([_["bads"] / _["freq"] for _ in l[1]]),
+          scipy.stats.spearmanr([_["bin"] for _ in l[1]], [_["bads"] / _["freq"] for _ in l[1]])[0]] 
+         for l in _l1]
+
+  _l3 = [l[0] for l in sorted(_l2, key = lambda x: -len(x[0])) 
+         if numpy.abs(round(l[3], 8)) == 1 and round(l[1], 8) > 0 and round(l[2], 8) < 1][0]
+
+  _l4 = sorted(*[l[1] for l in _l1 if l[0] == _l3], key = lambda x: x["bads"] / x["freq"])
+
+  if len([_ for _ in _data if _[2] == 0]) > 0:
+    _m1 = miss_bin([_[1] for _ in _data if _[2] == 0])
+    if _m1["bads"] == 0: 
+      for _ in ['freq', 'miss', 'bads']:
+        _l4[0][_]  = _l4[0][_]  + _m1[_] 
+    elif _m1["freq"] == _m1["bads"]: 
+      for _ in ['freq', 'miss', 'bads']:
+        _l4[-1][_]  = _l4[-1][_]  + _m1[_]
+    else:
+      _l4.append(_m1)
+
+  _l5 = sorted([{**_, "rate": round(_["bads"] / _["freq"], 4),
+                 "woe": round(numpy.log((_["bads"] / _bads) / ((_["freq"] - _["bads"]) / (_freq - _bads))), 4),
+                 "iv" : round((_["bads"] / _bads - (_["freq"] - _["bads"]) / (_freq - _bads)) * 
+                               numpy.log((_["bads"] / _bads) / ((_["freq"] - _["bads"]) / (_freq - _bads))), 4)} 
+                for _ in _l4], key = lambda x: x["bin"])
+
+  for _ in _l5:
+    if _["bin"] == 0:
+      _["rule"] = "numpy.isnan($X$)"
+    elif _["bin"] == len(_l3) + 1:
+      if _["miss"] == 0:
+        _["rule"] = "$X$ > " + str(_l3[-1])
+      else:
+        _["rule"] = "($X$ > " + str(_l3[-1]) + ") or numpy.isnan($X$)"
+    elif _["bin"] == 1:
+      if _["miss"] == 0:
+        _["rule"] = "$X$ <= " + str(_l3[0])
+      else:
+        _["rule"] = "($X$ <= " + str(_l3[0]) + ") or numpy.isnan($X$)"
+    else:
+        _["rule"] = "($X$ > " + str(_l3[_["bin"] - 2]) + ") and ($X$ <= " + str(_l3[_["bin"] - 1]) + ")"
+
+  _sel = ["bin", "freq", "miss", "bads", "rate", "woe", "iv", "rule"]
+
+  return({"cut": _l3, "tbl": [{k: _[k] for k in _sel} for _ in _l5]})
+  
+##################################################################################################
+
+def bad_bin(x, y):
+  """
+  The function discretizes the x vector based on percentiles and then summarizes over the y vector
+  with y = 1 to derive the weight of evidence transformaton (WoE) and information values.
+
+  Parameters:
+    x : A numeric vector to discretize.
+    y : A numeric vector with binary values of 0/1 and with the same length of x.
+
+  Returns:
+    A dictionary with two keys:
+      "cut" : a numeric vector with cut points applied to the x vector.
+      "tbl" : a list of dictionaries summarizing the binning outcome.
+
+  Example:
+    bin = bad_bin(derog, bad)
+
+    bin["cut"]
+    # [0.0, 2.0, 4.0]
+
+    import tabulate
+    print(tabulate.tabulate(bin['tbl'], headers = "keys", tablefmt = "simple"))
+    #   bin    freq    miss    bads    rate      woe      iv  rule
+    # -----  ------  ------  ------  ------  -------  ------  ---------------------------------
+    #     0     213     213      70  0.3286   0.6416  0.0178  numpy.isnan($X$)
+    #     1    2850       0     367  0.1288  -0.5559  0.1268  $X$ <= 0.0
+    #     2    1369       0     314  0.2294   0.144   0.0051  ($X$ > 0.0) and ($X$ <= 2.0)
+    #     3     587       0     176  0.2998   0.5078  0.0298  ($X$ > 2.0) and ($X$ <= 4.0)
+    #     4     818       0     269  0.3289   0.6426  0.0685  $X$ > 4.0
+  """
+
+  _data = [_ for _ in zip(x, y, ~numpy.isnan(x))]
+  _freq = len(_data)
+  _bads = sum([_[1] for _ in _data])
+
+  _x = [_[0] for _ in _data if _[2] == 1]
+  _y = [_[1] for _ in _data if _[2] == 1]
+
+  _n = numpy.arange(2, max(3, min(50, len(numpy.unique([_[0] for _ in _data if _[1] == 1 and _[2] == 1])) - 1)))
+  _p = numpy.unique([qcut([_[0] for _ in _data if _[1] == 1 and _[2] == 1], _) for _ in _n])
+
+  _l1 = [[_, manual_bin(_x, _y, _)] for _ in _p]
+
+  _l2 = [[l[0], min([_["bads"] / _["freq"] for _ in l[1]]), max([_["bads"] / _["freq"] for _ in l[1]]),
+          scipy.stats.spearmanr([_["bin"] for _ in l[1]], [_["bads"] / _["freq"] for _ in l[1]])[0]] 
+         for l in _l1]
+
+  _l3 = [l[0] for l in sorted(_l2, key = lambda x: -len(x[0])) 
+         if numpy.abs(round(l[3], 8)) == 1 and round(l[1], 8) > 0 and round(l[2], 8) < 1][0]
+
+  _l4 = sorted(*[l[1] for l in _l1 if l[0] == _l3], key = lambda x: x["bads"] / x["freq"])
+
+  if len([_ for _ in _data if _[2] == 0]) > 0:
+    _m1 = miss_bin([_[1] for _ in _data if _[2] == 0])
+    if _m1["bads"] == 0: 
+      for _ in ['freq', 'miss', 'bads']:
+        _l4[0][_]  = _l4[0][_]  + _m1[_] 
+    elif _m1["freq"] == _m1["bads"]: 
+      for _ in ['freq', 'miss', 'bads']:
+        _l4[-1][_]  = _l4[-1][_]  + _m1[_]
+    else:
+      _l4.append(_m1)
+
+  _l5 = sorted([{**_, "rate": round(_["bads"] / _["freq"], 4),
+                 "woe": round(numpy.log((_["bads"] / _bads) / ((_["freq"] - _["bads"]) / (_freq - _bads))), 4),
+                 "iv" : round((_["bads"] / _bads - (_["freq"] - _["bads"]) / (_freq - _bads)) * 
+                               numpy.log((_["bads"] / _bads) / ((_["freq"] - _["bads"]) / (_freq - _bads))), 4)} 
+                for _ in _l4], key = lambda x: x["bin"])
+
+  for _ in _l5:
+    if _["bin"] == 0:
+      _["rule"] = "numpy.isnan($X$)"
+    elif _["bin"] == len(_l3) + 1:
+      if _["miss"] == 0:
+        _["rule"] = "$X$ > " + str(_l3[-1])
+      else:
+        _["rule"] = "($X$ > " + str(_l3[-1]) + ") or numpy.isnan($X$)"
+    elif _["bin"] == 1:
+      if _["miss"] == 0:
+        _["rule"] = "$X$ <= " + str(_l3[0])
+      else:
+        _["rule"] = "($X$ <= " + str(_l3[0]) + ") or numpy.isnan($X$)"
+    else:
+        _["rule"] = "($X$ > " + str(_l3[_["bin"] - 2]) + ") and ($X$ <= " + str(_l3[_["bin"] - 1]) + ")"
+
+  _sel = ["bin", "freq", "miss", "bads", "rate", "woe", "iv", "rule"]
+
+  return({"cut": _l3, "tbl": [{k: _[k] for k in _sel} for _ in _l5]})
+  
+##################################################################################################
+
+def iso_bin(x, y):
+  """
+  The function discretizes the x vector based on the isotonic regression and then summarizes over the y vector
+  to derive the weight of evidence transformaton (WoE) and information values.
+
+  Parameters:
+    x : A numeric vector to discretize. 
+    y : A numeric vector with binary values of 0/1 and with the same length of x.
+
+  Returns:
+    A dictionary with two keys:
+      "cut" : a numeric vector with cut points applied to the x vector.
+      "tbl" : a list of dictionaries summarizing the binning outcome.
+
+  Example:
+    bin = iso_bin(derog, bad)
+
+    bin["cut"]
+    # [1.0, 2.0, 3.0]
+
+    import tabulate
+    print(tabulate.tabulate(bin['tbl'], headers = "keys", tablefmt = "simple")) 
+    #   bin    freq    miss    bads    rate      woe      iv  rule
+    # -----  ------  ------  ------  ------  -------  ------  ---------------------------------
+    #     0     213     213      70  0.3286   0.6416  0.0178  numpy.isnan($X$)
+    #     1    3741       0     560  0.1497  -0.3811  0.0828  $X$ <= 1.0
+    #     2     478       0     121  0.2531   0.274   0.0066  ($X$ > 1.0) and ($X$ <= 2.0)
+    #     3     332       0      86  0.259    0.305   0.0058  ($X$ > 2.0) and ($X$ <= 3.0)
+    #     4    1073       0     359  0.3346   0.6684  0.0978  $X$ > 3.0
+  """
+
+  _data = [_ for _ in zip(x, y, ~numpy.isnan(x))]
+  _freq = len(_data)
+  _bads = sum([_[1] for _ in _data])
+
+  _x = [_[0] for _ in _data if _[2] == 1]
+  _y = [_[1] for _ in _data if _[2] == 1]
+
+  _cor = scipy.stats.spearmanr(_x, _y)[0]
+  _reg = sklearn.isotonic.IsotonicRegression()
+
+  _f = numpy.abs(_reg.fit_transform(_x, list(map(lambda y:  y * _cor / numpy.abs(_cor), _y))))
+
+  _l1 = sorted(list(zip(_f, _x, _y)), key = lambda x: x[0])
+  _l2 = [[l for l in _l1 if l[0] == f] for f in sorted(set(_f))]
+  _l3 = [[*set(_[0] for _ in l), 
+          max(_[1] for _ in l), 
+          numpy.mean([_[2] for _ in l]),
+          sum(_[2] for _ in l)] for l in _l2]
+  
+  _p = sorted([_[1] for _ in [l for l in _l3 if l[2] < 1 and l[2] > 0 and l[3] > 10]])[1:-1]
+
+  _l4 = sorted(manual_bin(_x, _y, _p), key = lambda x: x["bads"] / x["freq"])
+
+  if len([_ for _ in _data if _[2] == 0]) > 0:
+    _m1 = miss_bin([_[1] for _ in _data if _[2] == 0])
+    if _m1["bads"] == 0: 
+      for _ in ['freq', 'miss', 'bads']:
+        _l4[0][_]  = _l4[0][_]  + _m1[_] 
+    elif _m1["freq"] == _m1["bads"]: 
+      for _ in ['freq', 'miss', 'bads']:
+        _l4[len(_l4) - 1][_]  = _l4[len(_l4) - 1][_]  + _m1[_]
+    else:
+      _l4 = [_m1] + _l4 
+
+  _l5 = sorted([{**_, 
+                 "rate": round(_["bads"] / _["freq"], 4),
+                 "woe" : round(numpy.log((_["bads"] / _bads) / ((_["freq"] - _["bads"]) / (_freq - _bads))), 4),
+                 "iv"  : round((_["bads"] / _bads - (_["freq"] - _["bads"]) / (_freq - _bads)) * 
+                               numpy.log((_["bads"] / _bads) / ((_["freq"] - _["bads"]) / (_freq - _bads))), 4)} 
+                for _ in _l4], key = lambda x: x["bin"])
+
+  for _ in _l5:
+    if _["bin"] == 0:
+      _["rule"] = "numpy.isnan($X$)"
+    elif _["bin"] == len(_p) + 1:
+      if _["miss"] == 0:
+        _["rule"] = "$X$ > " + str(_p[-1])
+      else:
+        _["rule"] = "($X$ > " + str(_p[-1]) + ") or numpy.isnan($X$)"
+    elif _["bin"] == 1:
+      if _["miss"] == 0:
+        _["rule"] = "$X$ <= " + str(_p[0])
+      else:
+        _["rule"] = "($X$ <= " + str(_p[0]) + ") or numpy.isnan($X$)"
+    else:
+        _["rule"] = "($X$ > " + str(_p[_["bin"] - 2]) + ") and ($X$ <= " + str(_p[_["bin"] - 1]) + ")"
+
+  _sel = ["bin", "freq", "miss", "bads", "rate", "woe", "iv", "rule"]
+
+  return({"cut": _p, "tbl": [{k: _[k] for k in _sel} for _ in _l5]})
+
+##################################################################################################
